673762325b0a90517ea26ac1aaf852566675917f
pkazmierczak <me@piotrkazmierczak.com>
2015-01-22 14:37:17 +0100

    ok, routes and forms basically kinda work

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 
 app = Flask(__name__)
@@ -7,10 +6,24 @@ app.config.from_object(__name__)
 
 
 @app.route('/')
-def hello(name=None):
+def show_main_page(name=None):
     return render_template('main.html', name=name)
 
 
-@app.route('/letters/<string:letters>')
-def show_results(letters):
-    return render_template('show_results.html', letters=letters)
+@app.route('/letters/', methods=['GET', 'POST'])
+def show_results():
+    user_input = request.form['letters']
+    solutions = compute_solution(user_input)
+    return render_template('show_results.html', solutions=solutions)
+
+
+def compute_solution(letters):
+    solutions = []
+    for letter in letters:
+        solutions.append(letter)
+    return solutions
+
+if __name__ == '__main__':
+    # TODO remove the debug flag before deployment!!!
+    app.debug = True
+    app.run()
